f2f9c0dc201e14151ad8b109616dc07c1f079f16
ub-victor <victoireushindi371@gmail.com>
2026-01-22 17:05:28 +0200

    Initial commit

--- new-react-app/src/App.jsx
+++ new-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App() { 
   const [counter, setCounter] = useState(0)
 
   return (
