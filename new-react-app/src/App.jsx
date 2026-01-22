5107fbc21160532566e0d32c6570f73fffcf0adf
ub-victor <victoireushindi371@gmail.com>
2026-01-22 15:08:16 +0200

    Initial commit

--- new-react-app/src/App.jsx
+++ new-react-app/src/App.jsx
@@ -3,21 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-const Person = (props) => {
-  
-  return(
-    <>
-    <h1>Name : {props.name}</h1>
-    <h2>Last name : {props.ln  }</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
-
 function App() {
   const [counter, setCounter] = useState(0)
-  
+
   return (
     <div>
       <Person
