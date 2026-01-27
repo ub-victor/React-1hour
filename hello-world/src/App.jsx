83cec40bfc8fd1cc53b7dacaa686c041aab8faed
ub-victor <victoireushindi371@gmail.com>
2026-01-27 14:32:27 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Greet from '../src/components/Greet'
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <Greet/>
+      <Greet name = "Victoire"/>
     </>
   )
 }
