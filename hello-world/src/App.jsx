b61609a643c3a26264a8c65a91725831467b159a
ub-victor <victoireushindi371@gmail.com>
2026-01-27 17:23:37 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
 
   return (
     <>
       <Message/>
+      <Counter/>
     </>
   )
 }
