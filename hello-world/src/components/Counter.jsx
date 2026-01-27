71f6a60d4431f9cfdde0f02e2f99e1a49546cd4f
ub-victor <victoireushindi371@gmail.com>
2026-01-27 17:29:28 +0200

    Initial commit

--- hello-world/src/components/Counter.jsx
+++ hello-world/src/components/Counter.jsx
@@ -4,7 +4,7 @@ const Counter = () => {
     const {count, setCount} = useState(0);
     return ( 
         <div>
-            <button>{count}</button>
+            <button onClick={()=>setCount((count)=>count++)}>{count}</button>
         </div>
 
 
