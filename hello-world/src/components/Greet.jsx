5921839ab485eae9aacf29cf9eef2e4fa6441fdb
ub-victor <victoireushindi371@gmail.com>
2026-01-27 23:03:14 +0200

    Initial commit

--- hello-world/src/components/Greet.jsx
+++ hello-world/src/components/Greet.jsx
@@ -1,8 +1,8 @@
-const Greet = () => {
+const Greet = ({name,heroName}) => {
     return ( 
         <div>
             <h1>
-                Hello {} a.k.a {}
+                Hello {name} a.k.a {heroName}
             </h1>
         </div>
      );
