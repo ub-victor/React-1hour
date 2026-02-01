386071b5d1f0273b78aabb5059edaafa54f0f606
ub-victor <victoireushindi371@gmail.com>
2026-02-01 14:26:15 +0200

    Initial commint

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './index'
+import './index.css'
 function App() {
+
+  const Card = ({title})
 
   return (
     
