3a16cf6672ac9ec2dd181a0ebd57474136ced1b6
ub-victor <victoireushindi371@gmail.com>
2026-02-01 14:51:35 +0200

    Initial commint

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className='card-container'>
       <Card title="Star Wars"/>
-      <Card title="Star Wars"/>
-      <Card title="Star Wars"/>
+      <Card title="Avatar"/>
+      <Card title="The Lion King"/>
 
     </div>
     
