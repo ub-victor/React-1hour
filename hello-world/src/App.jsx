43b3c1f3dd1f71d8f73a623a626063b78ff9c742
ub-victor <victoireushindi371@gmail.com>
2026-02-02 01:17:16 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Greet from '../src/components/Greet'
+import React from 'react'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <Greet name = "Victoire" heroName= "batman">
-        <p>This is children props</p>
-      </Greet>
-      <Greet name = "Ushindi" heroName= "ironman">
-        <button>Action</button>
-      </Greet>
-    </>
+    <div>
+      
+    </div>
   )
 }
 
