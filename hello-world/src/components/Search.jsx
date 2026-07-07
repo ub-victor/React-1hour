737b7918320608ad785a4650f2b0c08609f843cf
ub-victor <victoireushindi371@gmail.com>
2026-07-07 18:47:52 +0200

    Search added

--- hello-world/src/components/Search.jsx
+++ hello-world/src/components/Search.jsx
@@ -7,7 +7,7 @@ const Search = ({searchTerm, setSearchTerm}) => {
             <img src="search.svg" alt="search" />
             <input type="text"
             placeholder= "Search through thousand of movies"
-            value = { searchTerm }
+            value = {searchTerm}
             onChange={(e)=> setSearchTerm(e.target.value)}
             />
         </div>
